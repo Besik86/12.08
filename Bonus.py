b69131acc96d5fd5ea2ffd5a14baff06783c98b9
Besik86 <besik86@gmail.com>
2024-08-12 17:19:00 +0300

    Add files via upload

---
+++ Bonus.py
@@ -0,0 +1,6 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+A = sum(grades[0])/len(grades[0])
+print(A)
+s = sorted(students)
+print(s)
